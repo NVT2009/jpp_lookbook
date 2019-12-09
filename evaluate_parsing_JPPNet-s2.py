528ca2ab3dd94e58f7bd05cab3b4481a7df4620c
macuser <tiennv3@bap.jp>
2019-12-09 16:08:37 +0700

    Segemtation DeepFashion

---
+++ evaluate_parsing_JPPNet-s2.py
@@ -0,0 +1,178 @@
+from __future__ import print_function
+import argparse
+from datetime import datetime
+import os
+import sys
+import time
+import scipy.misc
+import cv2
+from PIL import Image
+from get_segment import check_full_body, check_background_color
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+from utils import *
+from LIP_model import *
+
+N_CLASSES = 20
+INPUT_SIZE = (384, 384)
+NUM_STEPS = 10000  # Number of images in the validation set.
+
+DATA_DIRECTORY = '/Users/macuser/Downloads/Deep_Fashion/In-shopclothes/'
+DATA_LIST_PATH = '/Users/macuser/Downloads/Deep_Fashion/list_eval_partition.txt'
+RESTORE_DIR = '/Users/macuser/Downloads/JPPNet-s2/model.ckpt-205632'
+OUTPUT_DIR = '/Users/macuser/Downloads/Deep_Fashion/In-shopclothes/output'
+
+if not os.path.exists(OUTPUT_DIR):
+    os.makedirs(OUTPUT_DIR)
+
+
+def main():
+    """Create the model and start the evaluation process."""
+
+    # Create queue coordinator.
+    coord = tf.train.Coordinator()
+    h, w = INPUT_SIZE
+    # Load reader.
+    with tf.name_scope("create_inputs"):
+        reader = ImageReader(DATA_DIRECTORY, DATA_LIST_PATH, None, False, False, coord)
+        image = reader.image
+        image_rev = tf.reverse(image, tf.stack([1]))
+        image_list = reader.image_list
+        NUM_STEPS = len(image_list)
+    image_batch_origin = tf.stack([image, image_rev])
+    image_batch = tf.image.resize_images(image_batch_origin, [int(h), int(w)])
+    image_batch075 = tf.image.resize_images(image_batch_origin, [int(h * 0.75), int(w * 0.75)])
+    image_batch125 = tf.image.resize_images(image_batch_origin, [int(h * 1.25), int(w * 1.25)])
+
+    # Create network.
+    with tf.variable_scope('', reuse=False):
+        net_100 = JPPNetModel({'data': image_batch}, is_training=False, n_classes=N_CLASSES)
+    with tf.variable_scope('', reuse=True):
+        net_075 = JPPNetModel({'data': image_batch075}, is_training=False, n_classes=N_CLASSES)
+    with tf.variable_scope('', reuse=True):
+        net_125 = JPPNetModel({'data': image_batch125}, is_training=False, n_classes=N_CLASSES)
+
+    # parsing net
+    parsing_fea1_100 = net_100.layers['res5d_branch2b_parsing']
+    parsing_fea1_075 = net_075.layers['res5d_branch2b_parsing']
+    parsing_fea1_125 = net_125.layers['res5d_branch2b_parsing']
+
+    parsing_out1_100 = net_100.layers['fc1_human']
+    parsing_out1_075 = net_075.layers['fc1_human']
+    parsing_out1_125 = net_125.layers['fc1_human']
+
+    # pose net
+    resnet_fea_100 = net_100.layers['res4b22_relu']
+    resnet_fea_075 = net_075.layers['res4b22_relu']
+    resnet_fea_125 = net_125.layers['res4b22_relu']
+
+    with tf.variable_scope('', reuse=False):
+        pose_out1_100, pose_fea1_100 = pose_net(resnet_fea_100, 'fc1_pose')
+        pose_out2_100, pose_fea2_100 = pose_refine(pose_out1_100, parsing_out1_100, pose_fea1_100, name='fc2_pose')
+        parsing_out2_100, parsing_fea2_100 = parsing_refine(parsing_out1_100, pose_out1_100, parsing_fea1_100,
+                                                            name='fc2_parsing')
+        parsing_out3_100, parsing_fea3_100 = parsing_refine(parsing_out2_100, pose_out2_100, parsing_fea2_100,
+                                                            name='fc3_parsing')
+
+    with tf.variable_scope('', reuse=True):
+        pose_out1_075, pose_fea1_075 = pose_net(resnet_fea_075, 'fc1_pose')
+        pose_out2_075, pose_fea2_075 = pose_refine(pose_out1_075, parsing_out1_075, pose_fea1_075, name='fc2_pose')
+        parsing_out2_075, parsing_fea2_075 = parsing_refine(parsing_out1_075, pose_out1_075, parsing_fea1_075,
+                                                            name='fc2_parsing')
+        parsing_out3_075, parsing_fea3_075 = parsing_refine(parsing_out2_075, pose_out2_075, parsing_fea2_075,
+                                                            name='fc3_parsing')
+
+    with tf.variable_scope('', reuse=True):
+        pose_out1_125, pose_fea1_125 = pose_net(resnet_fea_125, 'fc1_pose')
+        pose_out2_125, pose_fea2_125 = pose_refine(pose_out1_125, parsing_out1_125, pose_fea1_125, name='fc2_pose')
+        parsing_out2_125, parsing_fea2_125 = parsing_refine(parsing_out1_125, pose_out1_125, parsing_fea1_125,
+                                                            name='fc2_parsing')
+        parsing_out3_125, parsing_fea3_125 = parsing_refine(parsing_out2_125, pose_out2_125, parsing_fea2_125,
+                                                            name='fc3_parsing')
+
+    parsing_out1 = tf.reduce_mean(
+        tf.stack([tf.image.resize_images(parsing_out1_100, tf.shape(image_batch_origin)[1:3, ]),
+                  tf.image.resize_images(parsing_out1_075, tf.shape(image_batch_origin)[1:3, ]),
+                  tf.image.resize_images(parsing_out1_125, tf.shape(image_batch_origin)[1:3, ])]), axis=0)
+    parsing_out2 = tf.reduce_mean(
+        tf.stack([tf.image.resize_images(parsing_out2_100, tf.shape(image_batch_origin)[1:3, ]),
+                  tf.image.resize_images(parsing_out2_075, tf.shape(image_batch_origin)[1:3, ]),
+                  tf.image.resize_images(parsing_out2_125, tf.shape(image_batch_origin)[1:3, ])]), axis=0)
+    parsing_out3 = tf.reduce_mean(
+        tf.stack([tf.image.resize_images(parsing_out3_100, tf.shape(image_batch_origin)[1:3, ]),
+                  tf.image.resize_images(parsing_out3_075, tf.shape(image_batch_origin)[1:3, ]),
+                  tf.image.resize_images(parsing_out3_125, tf.shape(image_batch_origin)[1:3, ])]), axis=0)
+
+    raw_output = tf.reduce_mean(tf.stack([parsing_out1, parsing_out2, parsing_out3]), axis=0)
+    head_output, tail_output = tf.unstack(raw_output, num=2, axis=0)
+    tail_list = tf.unstack(tail_output, num=20, axis=2)
+    tail_list_rev = [None] * 20
+    for xx in range(14):
+        tail_list_rev[xx] = tail_list[xx]
+    tail_list_rev[14] = tail_list[15]
+    tail_list_rev[15] = tail_list[14]
+    tail_list_rev[16] = tail_list[17]
+    tail_list_rev[17] = tail_list[16]
+    tail_list_rev[18] = tail_list[19]
+    tail_list_rev[19] = tail_list[18]
+    tail_output_rev = tf.stack(tail_list_rev, axis=2)
+    tail_output_rev = tf.reverse(tail_output_rev, tf.stack([1]))
+
+    raw_output_all = tf.reduce_mean(tf.stack([head_output, tail_output_rev]), axis=0)
+    raw_output_all = tf.expand_dims(raw_output_all, dim=0)
+    raw_output_all = tf.argmax(raw_output_all, dimension=3)
+    pred_all = tf.expand_dims(raw_output_all, dim=3)  # Create 4-d tensor.
+
+    # Which variables to load.
+    restore_var = tf.global_variables()
+    # Set up tf session and initialize variables. 
+    config = tf.ConfigProto(log_device_placement=True, allow_soft_placement=True)
+    config.gpu_options.allow_growth = True
+    sess = tf.Session(config=config)
+    init = tf.global_variables_initializer()
+
+    sess.run(init)
+    sess.run(tf.local_variables_initializer())
+
+    # Load weights.
+    loader = tf.train.Saver(var_list=restore_var)
+
+    loader.restore(sess, RESTORE_DIR)
+    # Start queue threads.
+    threads = tf.train.start_queue_runners(coord=coord, sess=sess)
+
+    # Iterate over training steps.
+    for step in range(NUM_STEPS):
+        start = time.time()
+        parsing_ = sess.run(pred_all)
+
+        img_split = image_list[step].split('/')
+        img_id = img_split[-1][:-4]
+
+        msk = decode_labels(parsing_, num_classes=N_CLASSES)
+        parsing_im = Image.fromarray(msk[0])
+
+        if check_full_body(parsing_im):
+            if check_background_color(image_list[step], parsing_im):
+                output_dir = '/'.join(str(os.path.join(OUTPUT_DIR, reader.tmp)).split('/')[:-1]).replace('img/',
+                                                                                                              'seg/')
+                if not os.path.exists(output_dir):
+                    os.makedirs(output_dir)
+
+                parsing_im.save(os.path.join(OUTPUT_DIR, reader.tmp).replace('img/', 'seg/').replace('jpg', 'png'))
+                print("Step {} takes {} second(s) --- full_body -> Saved.".format(step, time.time() - start))
+                with open(os.path.join(OUTPUT_DIR, 'seg', 'fullbody_info.txt'), 'a') as writer:
+                    writer.write("{},{}\n".format(reader.tmp, reader.tmp.replace('img/', 'seg/')))
+        else:
+            print("Step {} takes {} second(s).".format(step, time.time() - start))
+
+    coord.request_stop()
+    coord.join(threads)
+
+
+if __name__ == '__main__':
+    main()
---
+++ get_segment.py
@@ -0,0 +1,49 @@
+import cv2
+import numpy as np
+
+# face, hair,
+upper_colour_label = [(255,0,0), (0,0,255)]
+# LeftLeg, RightLeg, LeftShoe, RightShoe
+lower_colour_label = [(170,255,85), (85,255,170),(0,255,255),(0,170,255)]
+
+
+def check_background_color(dir_image, parsing_img):
+    origin_image = cv2.imread(dir_image)
+    pil_image = parsing_img.convert('RGB')
+    open_cv_image = np.array(pil_image)
+    # Convert RGB to BGR
+    parsing_img = open_cv_image[:, :, ::-1].copy()
+
+    temp = cv2.inRange(parsing_img, (0,0,0), (0,0,0))
+    seg_binary = np.where((temp > 0),1,0).astype(dtype='uint8')
+    re_image = seg_binary[:,:, np.newaxis] * origin_image
+    num_range = np.mean(re_image)
+    if num_range > 127:
+        return True
+
+
+def check_full_body(parsing_img):
+    upper =[]
+    lower = []
+    pil_image = parsing_img.convert('RGB')
+    open_cv_image = np.array(pil_image)
+    # Convert RGB to BGR
+    parsing_img = open_cv_image[:, :, ::-1].copy()
+    for colour in upper_colour_label:
+        temp = cv2.inRange(parsing_img, colour, colour)
+        if len(np.nonzero(temp)) != 0:
+            upper.append(temp)
+
+    if len(upper) > 0:
+        for colour in lower_colour_label:
+            temp = cv2.inRange(parsing_img, colour, colour)
+            if len(np.nonzero(temp)) != 0:
+                lower.append(temp)
+
+        if len(lower) > 0:
+            return True
+        else:
+            return False
+    else:
+        return False
+
